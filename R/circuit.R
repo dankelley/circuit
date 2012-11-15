ae67c614e9824605391eacf0273b733389361cc2
Dan Kelley <kelley.dan@gmail.com>
2012-11-15 17:28:22 -0400

    .

--- R/circuit.R
+++ R/circuit.R
@@ -1,11 +1,11 @@
-circuit <- function(grid=FALSE, mar=rep(2, 4))
+circuit <- function(xlim=c(0,1), ylim=c(0,1), axes=FALSE, mar=rep(2, 4))
 {
     plot.new()
     par(mar=mar, mgp=c(2, 0.7, 0))
-    plot.window(c(0, 1), c(0, 1), asp=1)
-    if (grid) {
-        abline(h=seq(-2, 2, 0.1), lty='dotted', col='gray')
-        abline(v=seq(-2, 2, 0.1), lty='dotted', col='gray')
+    plot.window(xlim, ylim, asp=1)
+    if (axes) {
+        abline(h=seq(-5, 5, 0.1), lty='dotted', col='gray')
+        abline(v=seq(-5, 5, 0.1), lty='dotted', col='gray')
         axis(1)
         axis(2)
         box()
